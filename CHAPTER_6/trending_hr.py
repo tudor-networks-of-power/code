f0faa2c62226ee01694e6d0de970b0700f51e4dd
tudor-networks-of-power <124277017+tudor-networks-of-power@users.noreply.github.com>
2023-10-09 23:45:27 +0100

    Update trending_hr.py

--- CHAPTER_6/trending_hr.py
+++ CHAPTER_6/trending_hr.py
@@ -1,25 +1,19 @@
-#!/usr/bin/env python                                                           
+#!/usr/bin/env python                                                                                                                                
 import sys
 import os
 import random
 import networkx
 import operator
-#import matplotlib
-#import pylab
+#import matplotlib                                                                                                                                   
+#import pylab                                                                                                                                        
 import math
-#import cPickle
+#import cPickle                                                                                                                                      
 import time
 from collections import defaultdict
 
-# ANNUAL
+os.system('mkdir FREQ_HR_OUT')
+
+# ANNUAL                                                                                                                                             
 for year in range(1509,1604):
     curr = str(year)
     os.system('freq_hr - - '+curr+'0101 '+curr+'1231')
-
-"""
-# MONTHLY
-for year in range(1509,1604):
-    for month in range(1,13):
-        curr = str(year)+'0'[0:int(month < 10)]+str(month)
-        os.system('freq_hr - - '+curr+'00 '+curr+'99')
-"""
